98857b766d4461abae56f8b8ad5dd91e5f5c48ed
huynhnguyenday <nguyenhuuhuynh27022003@gmail.com>
2024-12-10 21:56:05 +0700

    update detail food

--- frontend/src/components/DetailFood.jsx
+++ frontend/src/components/DetailFood.jsx
@@ -58,7 +58,7 @@ const DetailFood = () => {
     return <div>Sản phẩm không tồn tại.</div>;
   }
 
-  const activeCategory = product.category; // Đảm bảo sản phẩm có thuộc tính category
+  const activeCategory = product.category.name; // Đảm bảo sản phẩm có thuộc tính category
 
   const handleCategoryClick = (categoryName) => {
     navigate(`/menu?category=${categoryName}`);
@@ -88,16 +88,25 @@ const DetailFood = () => {
         <img src={product.image} alt={product.name} />
       </div>
 
-      <div className="flex-1 space-y-4 detail-product">
-        <h1 className="text-3xl product-title">{product.name}</h1>
+      <div className="detail-product flex-1 space-y-4">
+        <h1 className="product-title text-3xl">{product.name}</h1>
         <p>
-          <span className="text-lg font-bold sell-price">{product.sell_price.toLocaleString()}đ</span>
-          <span className="line-through text-gray-500 mr-4 price">{product.price.toLocaleString()}đ</span>
+          <span className="sell-price text-lg font-bold">
+            {product.sell_price.toLocaleString()}đ
+          </span>
+          <span className="price mr-4 text-gray-500 line-through">
+            {product.price.toLocaleString()}đ
+          </span>
         </p>
         <div className="separator"></div>
         <span className="quantity-name">số lượng: </span>
         <div className="quantity-picker">
-          <button className="btn-minus-food" onClick={() => setQuantity((prev) => Math.max(prev - 1, 1))}>-</button>
+          <button
+            className="btn-minus-food"
+            onClick={() => setQuantity((prev) => Math.max(prev - 1, 1))}
+          >
+            -
+          </button>
           <input
             type="text"
             value={quantity}
@@ -105,21 +114,28 @@ const DetailFood = () => {
             onBlur={handleBlur}
             className="quantity-input"
           />
-          <button className="btn-plus-food" onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+          <button
+            className="btn-plus-food"
+            onClick={() => setQuantity((prev) => prev + 1)}
+          >
+            +
+          </button>
         </div>
         <button className="add-to-cart-btn">
           <FontAwesomeIcon icon={faBasketShopping} /> Thêm vào giỏ
         </button>
       </div>
 
-      <div className="flex-1 category">
+      <div className="category flex-1">
         <h3 className="df-category-name">Danh mục thực đơn</h3>
         <ul className="menu-list">
           {categories.map((category) => (
             <li
               key={category._id}
               onClick={() => handleCategoryClick(category.name)}
-              className={category.name === activeCategory ? "active-category" : ""}
+              className={
+                category.name === activeCategory ? "active-category" : ""
+              }
             >
               {category.name}
             </li>
